ec0538208553b28c43517d05c8f19d173b2ccf39
Вадим Евгеньевич Кавецкий <KVE@okbhmao.local>
2020-02-28 14:48:19 +0500

    Add lazyload

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,13 @@ function ibg() {
 	}
 }
 
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+	img.setAttribute('src', img.getAttribute('data-src'));
+	img.onload = function () {
+		img.removeAttribute('data-src');
+	};
+});
+
 function bindModal(triggerSelector, modalSelector, closeSelector) {
 	const trigger = document.querySelectorAll(triggerSelector),
 		modal = document.querySelector(modalSelector),
